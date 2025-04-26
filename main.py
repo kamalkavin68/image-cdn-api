ec1d64ae083b2ae3508dcb194b9008cd05fcea83
Kavin <kamalkavin68@gmail.com>
2025-04-26 08:42:39 +0530

    Add nifty.png image and refactor image retrieval endpoints for equity and indices

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi import HTTPException
-import shutil
 import os
 
 app = FastAPI()
 
 EQUITY_IMAGE_DIR = "images/equity/png"
+INDICES_IMAGE_DIR = "images/equity/png"
 
 @app.get("/")
 async def root():
     return {"detail": "API is running"}
 
 
-@app.get("/images/{image_name}")
-async def get_image(image_name: str):
-    extension = os.path.splitext(image_name)[1]
+@app.get("/images/equity/{image_name}")
+async def get_equity_image(image_name: str):
     image_path = EQUITY_IMAGE_DIR + "/" + image_name
+    print(f"Image path: {image_path}")
+    if not os.path.exists(image_path):
+        raise HTTPException(status_code=404, detail="Image not found")
+    return FileResponse(image_path, headers={
+        "Cache-Control": "public, max-age=31536000, immutable"
+    })
+
+@app.get("/images/indices/{image_name}")
+async def get_indices_image(image_name: str):
+    image_path = INDICES_IMAGE_DIR + "/" + image_name
     print(f"Image path: {image_path}")
     if not os.path.exists(image_path):
         raise HTTPException(status_code=404, detail="Image not found")
